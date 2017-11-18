d4b56bfc98de7050e9e5e80f7ff5876ebd6fd482
zhurou603 <526590351@qq.com>
2017-11-18 11:20:14 +0800

    first commit

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **argv)                                                                  
+    
+{                                                                           
+
+    std::cout << "enter two numbers" << std::endl;                          
+
+        int v1 = 0, v2= 0;                                                      
+
+        std::cin >> v1 >>v2;                                                    
+
+        std::cout << "the sum of " << v1 << "and" << v2 << "is" << v1 + v2 << std::endl;                            
+
+        return 0;
+
+}
